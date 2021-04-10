6e6b04140c217ad2b0d7c34632681a3c2138d2c7
Henry T <henry@henrytu.me>
2021-04-09 21:44:32 -0400

    Testing SSIM

--- quantitative_analysis/image_quality/ssim.py
+++ quantitative_analysis/image_quality/ssim.py
@@ -10,8 +10,39 @@ Structural similarity Analysis
 """
 
 import sys
+from skimage.metrics import structural_similarity as ssim
+import pickle
+import random
+from tqdm import tqdm
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: python3 %s <path to dataset pickle file>' % sys.argv[0])
+    if len(sys.argv) != 3:
+        print('Usage: python3 %s <path to real> <path to GAN>' % sys.argv[0])
         exit(1)
+
+    total_ssim = 0
+
+    with open(sys.argv[1], "rb") as real_file:
+        real_dataset = pickle.load(real_file)
+
+    with open(sys.argv[2], "rb") as gan_file:
+        gan_dataset = pickle.load(gan_file)
+
+    for i in tqdm(range(len(gan_dataset))):
+        img = gan_dataset[i]
+
+        real_image = real_dataset[random.randint(0, len(real_dataset) - 1)]
+
+        ssim_score = ssim(real_image, img, win_size=3, multichannel=True, data_range=img.max() - img.min())
+
+        if i % 100 == 0:
+            print('SSIM: %f; Running Average: %f' % (ssim_score, total_ssim / (i + 1)))
+
+        total_ssim += ssim_score
+
+    avg_ssim = total_ssim / len(gan_dataset)
+
+    print("Average SSIM: %f" % avg_ssim)
+
+    with open("ssim_result.txt", "w") as output_file:
+        output_file.write("Average SSIM: %f\n" % avg_ssim)
